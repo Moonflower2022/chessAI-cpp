d619d9e08426761cdaf179dd2c253778cf242564
Moonflower2022 <104652800+Moonflower2022@users.noreply.github.com>
2024-07-19 11:20:44 -0700

    added check to make sure that input move is valid (added more instruction as to how to input the move)

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A chess bot written in c++
 
 use `g++ -std=c++20 main.cpp -o out` to compile.  
+
+For usage, just do `./out` and then pull your terminal a bit higher to see all of the board.  
--- main.cpp
+++ main.cpp
@@ -5,26 +5,47 @@
 using namespace chess;
 using namespace std;
 
+Move get_user_move(Board board)
+{
+    Movelist possible_moves;
+    movegen::legalmoves(possible_moves, board);
+
+    while (true)
+    {
+        string input;
+        cout << "Input a move by providing the from square and the to square (for castling just input the king's movement): ";
+        cin >> input;
+        Move user_move = uci::uciToMove(board, input);
+
+        for (const Move move : possible_moves)
+        {
+            if (move == user_move)
+            {
+                return move;
+            }
+        }
+        cout << "Invalid move. Try again." << endl;
+        cout << board << endl;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     init_tables();
     Board board = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    pair<GameResultReason, GameResult> resultPair = board.isGameOver();
+    pair<GameResultReason, GameResult> result_pair = board.isGameOver();
     cout << board << endl;
-    while (resultPair.second == GameResult::NONE)
+    while (result_pair.second == GameResult::NONE)
     {
-        string input;
-        cout << "Input a move: "; // Type a number and press enter
-        cin >> input;
-        Move userMove = uci::uciToMove(board, input);
-
-        board.makeMove(userMove);
+        Move user_move = get_user_move(board);
+        board.makeMove(user_move);
         cout << board << endl;
 
         Move computer_move = minimax(4, board, -MAX, MAX, false).second;
 
         board.makeMove(computer_move);
         cout << board << endl;
+        cout << "Black's move: " << uci::moveToUci(computer_move) << endl;
     }
 
     return 0;
