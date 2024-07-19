8ece194d27ab855cd384ec38772da08d292ce862
Moonflower2022 <104652800+Moonflower2022@users.noreply.github.com>
2024-07-19 12:34:16 -0700

    wrote a test file for the sake of profiling

---
+++ test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "chess.hpp"
+#include "bot.cpp"
+#include "evaluation.cpp"
+
+using namespace chess;
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    init_tables();
+    Board board = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    pair<GameResultReason, GameResult> result_pair = board.isGameOver();
+    cout << board << endl;
+
+    vector<string> input_moves = {"a2a3", "b2b3", "c2c3", "d2d3", "e2e3", "f2f3", "g2g3", "h2h3"};
+
+    for (const string move : input_moves)
+    {
+        board.makeMove(uci::uciToMove(board, move));
+        cout << board << endl;
+
+        Move computer_move = minimax(4, board, -MAX, MAX, false).second;
+
+        board.makeMove(computer_move);
+        cout << board << endl;
+        cout << "Black's move: " << uci::moveToUci(computer_move) << endl;
+    }
+
+    return 0;
+}
